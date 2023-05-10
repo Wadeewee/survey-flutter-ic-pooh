6a19cb5b85b8e2e3bafb2202aa0bcc029b63e6dd
Wadeewee <suphawadee@nimblehq.co>
2023-05-10 15:28:32 +0700

    [#33] Create AppRouter and inject getIt

--- lib/navigation/route.dart
+++ lib/navigation/route.dart
@@ -1,31 +1,40 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:injectable/injectable.dart';
 import 'package:survey_flutter_ic/ui/home/home_screen.dart';
 import 'package:survey_flutter_ic/ui/signin/sign_in_screen.dart';
 import 'package:survey_flutter_ic/ui/splash/splash_screen.dart';
 
-const routePathRootScreen = '/';
-const routePathHomeScreen = '/home';
-const routePathSignInScreen = '/sign_in';
+enum RoutePath {
+  root('/'),
+  home('/home'),
+  signIn('/sign_in');
 
-// TODO Extract to class and inject by GetIt
-GoRouter router([String? initialLocation]) => GoRouter(
-      initialLocation: initialLocation ?? routePathRootScreen,
-      routes: <GoRoute>[
-        GoRoute(
-          path: routePathRootScreen,
-          builder: (BuildContext context, GoRouterState state) =>
-              const SplashScreen(),
-        ),
-        GoRoute(
-          path: routePathSignInScreen,
-          builder: (BuildContext context, GoRouterState state) =>
-              const SignInScreen(),
-        ),
-        GoRoute(
-          path: routePathHomeScreen,
-          builder: (BuildContext context, GoRouterState state) =>
-              const HomeScreen(),
-        ),
-      ],
-    );
+  const RoutePath(this.path);
+
+  final String path;
+}
+
+@Singleton()
+class AppRouter {
+  GoRouter router([String? initialLocation]) => GoRouter(
+        initialLocation: initialLocation ?? RoutePath.root.path,
+        routes: <GoRoute>[
+          GoRoute(
+            path: RoutePath.root.path,
+            builder: (BuildContext context, GoRouterState state) =>
+                const SplashScreen(),
+          ),
+          GoRoute(
+            path: RoutePath.home.path,
+            builder: (BuildContext context, GoRouterState state) =>
+                const SignInScreen(),
+          ),
+          GoRoute(
+            path: RoutePath.signIn.path,
+            builder: (BuildContext context, GoRouterState state) =>
+                const HomeScreen(),
+          ),
+        ],
+      );
+}
