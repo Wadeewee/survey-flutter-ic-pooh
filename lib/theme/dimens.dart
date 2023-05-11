62ae6175a7bdb03923cdb5691d92c066f2dd3ed8
Wadeewee <suphawadee@nimblehq.co>
2023-05-11 16:53:45 +0700

    [#2] UI list of surveys home screen

--- lib/theme/dimens.dart
+++ lib/theme/dimens.dart
@@ -1,5 +1,7 @@
 const double space4 = 4.0;
+const double space5 = 5.0;
 const double space10 = 10.0;
+const double space15 = 15.0;
 const double space16 = 16.0;
 const double space18 = 18.0;
 const double space20 = 20.0;
@@ -10,6 +12,8 @@ const double borderRadius12 = 12.0;
 const double fontSize13 = 13.0;
 const double fontSize16 = 16.0;
 const double fontSize17 = 17.0;
+const double fontSize28 = 28.0;
 const double fontSize34 = 34.0;
 
 const double circleAvatarProfileSize = 36.0;
+const double pagerIndicatorSize = 8.0;
